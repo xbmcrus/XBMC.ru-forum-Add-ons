31f11b82fc907bba678d60adb970b2b7886cf1d0
bigbax <xbmcrus@gmail.com>
2019-01-11 16:28:37 +0300

    all broken addons deleted

--- script.module.translit/default.py
+++
@@ -1,9 +0,0 @@
-import sys
-import xbmc
-import xbmcgui
-import xbmcplugin
-import xbmcaddon
-
-settings = xbmcaddon.Addon(id='script.module.translit')
-version = "2.0.1"
-plugin = "Translit-" + version
